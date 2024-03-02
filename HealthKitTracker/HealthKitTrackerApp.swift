1b043de6ea83fc5bf3b2caeb944fa777baf679aa
Jericho Orienza <jkr.orienza@gmail.com>
2024-03-02 13:10:02 -0500

    Initial Commit

---
+++ HealthKitTracker/HealthKitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthKitTrackerApp.swift
+//  HealthKitTracker
+//
+//  Created by Jericho Orienza on 3/2/24.
+//
+
+import SwiftUI
+
+@main
+struct HealthKitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
