9a241fa3d4bd23bb3ef42aa6b1fa08400eacb392
Jericho Orienza <jkr.orienza@gmail.com>
2024-03-04 22:13:48 -0500

    implemented feature to track burned calories -- removed onAppear in HomeView and instead called functions to display work out cards when initializing HealthTracker

--- HealthKitTracker/HealthTracker.swift
+++ HealthKitTracker/HealthTracker.swift
@@ -14,19 +14,33 @@ extension Date {
     }
 }
 
+extension Double {
+    func formattedString() -> String  {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        numberFormatter.maximumFractionDigits = 0
+        
+        return numberFormatter.string(from: NSNumber(value: self))!
+    }
+}
+
 class HealthTracker: ObservableObject {
     
     let healthStore = HKHealthStore()
     
+    @Published var workouts: [String : WorkOut] = [:]
+    
     init() {
         let steps = HKQuantityType(.stepCount)
+        let calories = HKQuantityType(.activeEnergyBurned)
         
         let healthTypes: Set = [steps]
         
         Task {
             do {
                 try await healthStore.requestAuthorization(toShare: [], read: healthTypes)
-
+                getTodaySteps()
+                getTodayCalories()
             } catch {
                 print("error fetching health data")
             }
@@ -35,6 +49,7 @@ class HealthTracker: ObservableObject {
     
     func getTodaySteps() {
         let steps = HKQuantityType(.stepCount)
+        let calories = HKQuantityType(.activeEnergyBurned)
         let predicate = HKQuery.predicateForSamples(withStart: .startOfDay, end: Date())
         let query = HKStatisticsQuery(quantityType: steps, quantitySamplePredicate: predicate) { _, result, error in
             guard let quantity = result?.sumQuantity(), error == nil else{
@@ -42,11 +57,36 @@ class HealthTracker: ObservableObject {
                 return
             }
             let stepCount = quantity.doubleValue(for: .count())
-            print(stepCount)
+            let workout = WorkOut(id: 0, title: "Today Steps", subtitle: "Goal: 10,000", image: "figure.walk", amount: stepCount.formattedString())
+            DispatchQueue.main.async{
+                self.workouts["todaySteps"] = workout
+            }
+            print(stepCount.formattedString())
+        }
+        
+        healthStore.execute(query)
+    }
+    
+    func getTodayCalories() {
+        let calories = HKQuantityType(.activeEnergyBurned)
+        let predicate = HKQuery.predicateForSamples(withStart: .startOfDay, end: Date())
+        let query = HKStatisticsQuery(quantityType: calories, quantitySamplePredicate: predicate) { _, result, error in
+            guard let quantity = result?.sumQuantity(), error == nil else {
+                print("error fetching todays calorie data")
+                return
+            }
+            
+            let burnedCalories = quantity.doubleValue(for: .kilocalorie())
+            let workout = WorkOut(id: 1, title: "Today Calories", subtitle: "Goal: 360", image: "flame", amount: burnedCalories.formattedString())
+            DispatchQueue.main.async{
+                self.workouts["todayCals"] = workout
+            }
+            print(burnedCalories.formattedString())
         }
         
         healthStore.execute(query)
     }
 }
+
 
 
