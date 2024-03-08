2a455a7f72fd263b28e3573bdd48afc9cbb3b99a
Jericho Orienza <jkr.orienza@gmail.com>
2024-03-07 23:53:53 -0500

    creted framework to add more workouts easily & update UI -- playing with animations

--- HealthKitTracker/Preview Content/HomeView.swift
+++ HealthKitTracker/Preview Content/HomeView.swift
@@ -9,8 +9,18 @@ import SwiftUI
 
 struct HomeView: View {
     @EnvironmentObject var tracker: HealthTracker
+    let welcomeArray = ["Welcome", "Bienvenido", "Bienvenue"]
+    @State private var currentIndex = 0
     var body: some View {
-        VStack{
+        VStack(alignment: .leading) {
+            Text(welcomeArray[currentIndex])
+                .font(.largeTitle)
+                .padding()
+                .foregroundColor(.secondary)
+                .animation(.easeInOut(duration: 1), value: currentIndex)
+                .onAppear(){
+                    startWelcomeTimer()
+                }
             LazyVGrid(columns: Array(repeating: GridItem(spacing: 20), count: 2)) {
                 ForEach(tracker.workouts.sorted(by: {$0.value.id < $1.value.id}), id: \.key){ item in
                     WorkOutCard(workout: item.value)
@@ -18,7 +28,15 @@ struct HomeView: View {
             }
             .padding(.horizontal)
         }
-        //removed onAppear becasue we are instead calling it in HealthTracker when we init
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+    }
+    
+    func startWelcomeTimer(){
+        Timer.scheduledTimer(withTimeInterval: 3, repeats: true) { _ in
+            withAnimation{
+                currentIndex = (currentIndex + 1) % welcomeArray.count
+            }
+        }
     }
 }
 
@@ -26,5 +44,6 @@ struct HomeView: View {
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View{
         HomeView()
+            .environmentObject(HealthTracker()) //allow us to see home screen even without mocked data
     }
 }
